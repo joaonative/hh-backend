853248520f3e8fa39a3034d0a72cc6075b1b6b94
joaonative <jestudos127@gmail.com>
2024-01-16 15:20:42 -0300

    feat: new user habit & habitlog entities

---
+++ src/entities/Habit.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+} from "typeorm";
+import { User } from "./User";
+import { HabitLog } from "./HabitLog";
+
+@Entity()
+export class Habit {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ length: 100 })
+  name?: string;
+
+  @Column({ length: 50 })
+  description?: string;
+
+  @Column()
+  frequency?: number;
+
+  @Column({ default: 0 })
+  monthlyOccurrences?: number;
+
+  @ManyToOne(() => User, (user) => user.habits)
+  user?: User;
+
+  @OneToMany(() => HabitLog, (habitLog) => habitLog.habit)
+  habitLogs?: HabitLog[];
+}
---
+++ src/entities/HabitLog.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
+import { Habit } from "./Habit";
+
+@Entity()
+export class HabitLog {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  timestamp?: Date;
+
+  @Column()
+  notes?: string;
+
+  @ManyToOne(() => User, (user) => user.habitLogs)
+  user?: User;
+
+  @ManyToOne(() => Habit, (habit) => habit.habitLogs)
+  habit?: Habit;
+}
