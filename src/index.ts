0677830b101dc931cbd1a2da053c866e282dce73
joaonative <jestudos127@gmail.com>
2024-01-22 10:03:00 -0300

    new dep: cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import { mongooseConnection } from "./mongoose";
 
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", router);
 
