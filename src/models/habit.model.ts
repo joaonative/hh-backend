961d09ab358431003980b40907fd31f11624b2af
joaonative <jestudos127@gmail.com>
2024-01-29 17:35:26 -0300

    feat: new properties in habit

--- src/models/habit.model.ts
+++ src/models/habit.model.ts
@@ -6,17 +6,23 @@ export interface HabitDocument extends Document {
   name: string;
   description: string;
   frequency: number;
+  goal: number;
   monthlyOccurrences: number;
   totalOccurrences: number;
+  lastPerformed: Date | null;
+  isGood: boolean;
 }
 
 const habitSchema = new Schema<HabitDocument>({
   author: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
-  frequency: { type: Number, required: true },
+  frequency: { type: Number },
+  goal: { type: Number, required: true },
   monthlyOccurrences: { type: Number, default: 0 },
   totalOccurrences: { type: Number, default: 0 },
+  lastPerformed: { type: Date, default: null },
+  isGood: { type: Boolean, default: true },
 });
 
 habitSchema.path("author").validate(async function (value: string) {
