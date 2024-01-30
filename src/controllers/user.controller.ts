828bd0e9c5cc96758a6ea5da3b7ed8dc5826e6e5
joaonative <jestudos127@gmail.com>
2024-01-30 09:59:51 -0300

    feat: new routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -60,6 +60,50 @@ class UserController {
       res.status(500).send("Internal server error");
     }
   }
+
+  async getMonthlyStats(req: Request, res: Response) {
+    try {
+      const userId = req.params.userId;
+
+      // Encontrar o usuário pelo ID
+      const user = await UserModel.findById(userId).populate("habits");
+
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+
+      if (!user.habits || user.habits.length === 0) {
+        return res.status(404).send("User doesn't have any habits");
+      }
+
+      // Calcular a soma de monthlyOccurrences para todos os hábitos do usuário
+      const totalMonthlyOccurrences = user.habits.reduce(
+        (sum, habit) => sum + habit.monthlyOccurrences,
+        0
+      );
+
+      // Calcular a soma de monthlyOccurrences para hábitos com isGood igual a true
+      const totalMonthlyOccurrencesGoodHabits = user.habits.reduce(
+        (sum, habit) => (habit.isGood ? sum + habit.monthlyOccurrences : sum),
+        0
+      );
+
+      // Calcular a soma de monthlyOccurrences para hábitos com isGood igual a false
+      const totalMonthlyOccurrencesBadHabits = user.habits.reduce(
+        (sum, habit) => (!habit.isGood ? sum + habit.monthlyOccurrences : sum),
+        0
+      );
+
+      res.status(200).json({
+        totalMonthlyOccurrences,
+        totalMonthlyOccurrencesGoodHabits,
+        totalMonthlyOccurrencesBadHabits,
+      });
+    } catch (error) {
+      console.error("Error while fetching habits summary", error);
+      res.status(500).send("Internal server error");
+    }
+  }
 }
 
 export default new UserController();
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,14 +4,20 @@ import verifyToken from "./auth.middleware";
 import UserController from "../controllers/user.controller";
 import HabitController from "../controllers/habit.controller";
 import HabitLogController from "../controllers/habit-log.controller";
+import userController from "../controllers/user.controller";
+import habitController from "../controllers/habit.controller";
 
 const router = express.Router();
 
 router.post("/users", UserController.createUser); //create user
 
+router.get("/users", verifyToken, userController.getMonthlyStats); //get stats
+
 router.post("/habits", verifyToken, HabitController.createHabit); //create habit
 
 router.get("/habits", verifyToken, HabitController.getHabitsByUser); //get user habits
+
+router.delete("/habits/:id", verifyToken, habitController.deleteHabit); //delete user habit
 
 router.put("/habits/:id", verifyToken, HabitController.markHabitDoneToday); //mark habit done
 
